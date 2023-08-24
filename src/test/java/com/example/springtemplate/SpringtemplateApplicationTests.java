a249e38e55168cfefeae432ed230292c4bbd9bb4
kundan giri pavan bommadevara <kundanbommadevara@gmail.com>
2023-08-24 11:54:44 +0530

    initial commit

---
+++ src/test/java/com/example/springtemplate/SpringtemplateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springtemplate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringtemplateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
